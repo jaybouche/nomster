7396aa344a3d8b3e2cc3791cca8eac987c04a695
jaybouche <jaybouche@gmail.com>
2019-06-26 22:44:33 +0000

    10

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,9 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    @places = Place.all.page(params[ :page]).per_page(2)
+  end
+  def new
+    @place = Place.new
   end
 end
