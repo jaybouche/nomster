acca67688761982a5ba71f1062ae92d576f872d5
jaybouche <jaybouche@gmail.com>
2019-07-14 00:10:12 +0000

    18

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-
+  before_action :authenticate_user!, only: [:new, :create]
   def index
     @places = Place.all.page(params[ :page]).per_page(2)
   end
@@ -7,7 +7,7 @@ class PlacesController < ApplicationController
     @place = Place.new
   end
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
   private
